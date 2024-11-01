9697718a32650c8550688cb04b1bfb9d39e5e399
d.dasilva <daniel.hsilva@totvs.com.br>
2024-10-31 21:45:06 -0300

    Criando DTO Usuario

---
+++ src/main/java/br/com/tarefa/dtos/UsuarioDTO.java
@@ -0,0 +1,31 @@
+package br.com.tarefa.dtos;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@EqualsAndHashCode
+@NoArgsConstructor
+@AllArgsConstructor
+public class UsuarioDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private UUID id;
+	private String nome;
+	private String nomeUsuario;
+	private String senha;
+	private LocalDateTime dataCriacao;
+	private LocalDateTime dataAtualizacao;
+
+}
