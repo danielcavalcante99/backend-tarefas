c24a9051b379c5d7c5e79aded9b6624a017c30ce
d.dasilva <daniel.hsilva@totvs.com.br>
2024-10-31 21:41:06 -0300

    Criando DTO CriarTarefa

---
+++ src/main/java/br/com/tarefa/dtos/CriarTarefaDTO.java
@@ -0,0 +1,33 @@
+package br.com.tarefa.dtos;
+
+import java.io.Serializable;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+public class CriarTarefaDTO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	@Size(max = 50, message = "O campo 'titulo' é permitido um máximo de 50 caracteres")
+	@NotBlank(message = "Campo 'titulo' é obrigatório")
+	private String titulo;
+	
+	@Size(max = 250, message = "O campo 'descricao' é permitido um máximo de 250 caracteres")
+	@NotBlank(message = "Campo 'descricao' é obrigatório")
+	private String descricao;
+	
+}
