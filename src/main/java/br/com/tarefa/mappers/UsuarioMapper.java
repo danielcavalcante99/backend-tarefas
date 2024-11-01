9ef300518dafb620bbd55a037d8f84c3671bdc27
d.dasilva <daniel.hsilva@totvs.com.br>
2024-10-31 21:47:07 -0300

    Mapper usuário

---
+++ src/main/java/br/com/tarefa/mappers/UsuarioMapper.java
@@ -0,0 +1,23 @@
+package br.com.tarefa.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+import br.com.tarefa.dtos.CriarUsuarioDTO;
+import br.com.tarefa.dtos.UsuarioDTO;
+import br.com.tarefa.entities.Usuario;
+
+@Mapper(componentModel = "spring")
+public interface UsuarioMapper {
+	
+	@Mapping(target = "tarefas", ignore = true)
+	Usuario UsuarioDTOToUsuario(UsuarioDTO dto);
+	
+	UsuarioDTO UsuarioToUsuarioDTO(Usuario entity);
+	
+	@Mapping(target = "id", ignore = true)
+	@Mapping(target = "dataCriacao", ignore = true)
+	@Mapping(target = "dataAtualizacao", ignore = true)
+	Usuario criarUsuarioDTOToUsuario(CriarUsuarioDTO dto);
+
+}
