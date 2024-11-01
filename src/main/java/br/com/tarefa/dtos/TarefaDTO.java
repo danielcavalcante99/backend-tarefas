6b25845d2fadec114476d5a7593c3c2d27e83b97
d.dasilva <daniel.hsilva@totvs.com.br>
2024-10-31 21:44:18 -0300

    Criando DTO Tarefa

---
+++ src/main/java/br/com/tarefa/dtos/TarefaDTO.java
@@ -0,0 +1,34 @@
+package br.com.tarefa.dtos;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.springframework.hateoas.RepresentationModel;
+
+import br.com.tarefa.entities.enums.StatusTarefa;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.With;
+
+@Getter
+@Setter
+@Builder
+@With
+@AllArgsConstructor
+@NoArgsConstructor(staticName = "create")
+public class TarefaDTO extends RepresentationModel<TarefaDTO>  implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private UUID id;
+	private String titulo;
+	private String descricao;
+	private LocalDateTime dataCriacao;
+	private LocalDateTime dataAtualizacao;
+	private StatusTarefa status;
+	private UUID usuarioId;
+}
