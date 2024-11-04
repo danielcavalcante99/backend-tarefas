34da5a9bf22e5ab8e18a6bf60a3314d558d7e7a8
danielcavalcante99 <daniel16henrrique@gmail.com>
2024-11-04 02:01:41 -0300

    Ativando Caching, WebSecurity e MethodSecurity

--- src/main/java/br/com/tarefa/TarefaApplication.java
+++ src/main/java/br/com/tarefa/TarefaApplication.java
@@ -2,7 +2,13 @@ package br.com.tarefa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
+@EnableCaching
+@EnableWebSecurity
+@EnableMethodSecurity
 @SpringBootApplication
 public class TarefaApplication {
 
